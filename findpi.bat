2ec26b16378b204733fe102c1ca8fe2fd22d3549
Christian <142540893+comdif@users.noreply.github.com>
2025-12-08 08:11:33 +0100

    Modify ARP search for all Raspberry Pi Foundation MAC addresses
    Updated ARP commands to search for additional MAC addresses and display results.
    support for all Raspberry Pi Foundation Adresses

--- findpi.bat
+++ findpi.bat
@@ -6,11 +6,19 @@ for %%i in (%subip%) do set subip=%%~ni
 echo Premiere etape decouverte reseau %subip% soyez patient && echo First step network discovering %subip% be patient
 for /L %%i in (0,1,255) do @ping -n 1 -l 1 -w 1 %subip%.%%i -4 | findstr -m "=32"
 
-FOR /F "delims=" %%r IN ('"arp -a|findstr b8-27-eb"') DO SET result1=%%r
-FOR /F "delims=" %%s IN ('"arp -a|findstr dc-a6-32"') DO SET result2=%%s
+FOR /F "delims=" %%r IN ('"arp -a|findstr 28-cd-c1"') DO SET result1=%%r
+FOR /F "delims=" %%s IN ('"arp -a|findstr d8-3a-dd"') DO SET result2=%%s
+FOR /F "delims=" %%t IN ('"arp -a|findstr b8-27-eb"') DO SET result3=%%t
+FOR /F "delims=" %%u IN ('"arp -a|findstr dc-a6-32"') DO SET result4=%%u
+FOR /F "delims=" %%v IN ('"arp -a|findstr e4-5f-01"') DO SET result5=%%v
+
 
 echo ####################### RASPBERRY #######################
 IF NOT "%result1%"=="" ECHO %result1%
 IF NOT "%result2%"=="" ECHO %result2%
+IF NOT "%result3%"=="" ECHO %result3%
+IF NOT "%result4%"=="" ECHO %result4%
+IF NOT "%result5%"=="" ECHO %result5%
 echo #########################################################
+
 pause
