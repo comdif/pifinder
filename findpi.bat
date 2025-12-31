8e4d0439e73a1d5bcd05c3af3c5bacf0bc860ee2
Christian <142540893+comdif@users.noreply.github.com>
2025-12-31 09:26:28 +0100

    Update findpi.bat to improve output messages

--- findpi.bat
+++ findpi.bat
@@ -3,7 +3,7 @@ cls
 for /f "tokens=2,3 delims={,}" %%a in ('"WMIC NICConfig where IPEnabled="True" get DefaultIPGateway /value | find "I" "') do set mip=%%~a
 for /f "delims=" %%i in ('echo %mip%') do set subip=%%i
 for %%i in (%subip%) do set subip=%%~ni
-echo Premiere etape decouverte reseau %subip% soyez patient && echo First step network discovering %subip% be patient
+echo First step network discovering %subip% be patient
 for /L %%i in (0,1,255) do @ping -n 1 -l 1 -w 1 %subip%.%%i -4 | findstr -m "=32"
 
 ## TEST All Raspberry PI MAC sourced from Wiresharck ##
@@ -15,9 +15,8 @@ FOR /F "delims=" %%v IN ('"arp -a|findstr b8-27-eb"') DO SET result5=%%v
 FOR /F "delims=" %%w IN ('"arp -a|findstr d8-3a-dd"') DO SET result6=%%w
 FOR /F "delims=" %%x IN ('"arp -a|findstr dc-a6-32"') DO SET result7=%%x
 FOR /F "delims=" %%y IN ('"arp -a|findstr e4-5f-01"') DO SET result8=%%y
-88:a2:9e
 
-echo ####################### RASPBERRY #######################
+echo ####################### RASPBERRY FOUND #######################
 IF NOT "%result1%"=="" ECHO %result1%
 IF NOT "%result2%"=="" ECHO %result2%
 IF NOT "%result3%"=="" ECHO %result3%
@@ -26,7 +25,7 @@ IF NOT "%result5%"=="" ECHO %result5%
 IF NOT "%result6%"=="" ECHO %result6%
 IF NOT "%result7%"=="" ECHO %result7%
 IF NOT "%result8%"=="" ECHO %result8%
-echo #########################################################
+echo ###############################################################
 
 
 pause
